91e30447f8a9e48c4464b76765db073cd02702b2
Ahickling04 <ianhickling0@gmail.com>
2025-11-14 11:37:15 +0000

    removed uneeded code

--- noughts and crosses.cpp
+++ noughts and crosses.cpp
@@ -4,44 +4,6 @@
 #include <string>
 #include <conio.h>
 
-//int menu();
-//
-//int main(){
-//	//int occupiedArea[3][3] = {
-//	//	{0, 0, 0},
-//	//	{0, 0, 0},
-//	//	{0, 0, 0}
-//	//};
-//
-//	while (menu() != 3) {
-//		menu();
-//	}
-//}
-//
-//int menu() {
-//	
-//	int userInput;
-//	
-//	std::cout << "Welcome to Noughts and Crosses!\n";
-//	std::cout << "What would you like to do?\n1. Play 2P mode\n2. Play 1P mode (unavailable)\n3. Close program\n";
-//	std::cin >> userInput;
-//
-//	switch (userInput) {
-//	case 1:
-//		return 1;
-//		break;
-//	case 2:
-//		return 2;
-//		break;
-//	case 3:
-//		return 3;
-//		break;
-//	default:
-//		std::cout << "Invalid option\n";
-//		return -1;
-//	};
-//}
-
 using namespace std;
 
 void displayGrid();
